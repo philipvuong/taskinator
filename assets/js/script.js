c386ab13899b2b36ec5baab1cc994c74974f34e8
Philip Vuong <philipvuong@Homes-MacBook-Pro.local>
2020-05-15 14:54:12 -0500

    drag

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,13 +47,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
 };
 
 var createTaskEl = function(taskDataObj) {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
+  var listItemEl = document.createElement('li');
+  listItemEl.className = 'task-item';
 
-  listItemEl.setAttribute("data-task-id", taskIdCounter);
+  listItemEl.setAttribute('data-task-id', taskIdCounter);
+  listItemEl.setAttribute('draggable', 'true')
 
-  var taskInfoEl = document.createElement("div");
-  taskInfoEl.className = "task-info";
+  var taskInfoEl = document.createElement('div');
+  taskInfoEl.className = 'task-info';
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
   listItemEl.appendChild(taskInfoEl);
@@ -150,17 +151,33 @@ var taskStatusChangeHandler = function(event) {
   // find the parent task item element based on the id
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
 
-  if (statusValue === "to do") {
+  if (statusValue === 'to do') {
     tasksToDoEl.appendChild(taskSelected);
   } 
-  else if (statusValue === "in progress") {
+  else if (statusValue === 'in progress') {
     tasksInProgressEl.appendChild(taskSelected);
   } 
-  else if (statusValue === "completed") {
+  else if (statusValue === 'completed') {
     tasksCompletedEl.appendChild(taskSelected);
   }
 };
 
-formEl.addEventListener("submit", taskFormHandler);
+var dragTaskHandler = function(event) {
+  var taskId = event.target.getAttribute('data-task-id');
+  event.dataTransfer.setData('text/plain', taskId)
+  
+  var getId = event.dataTransfer.getData('text/plain');
+}
+
+var dropZoneDragHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    event.preventDefault();
+  }
+};
+
+formEl.addEventListener('submit', taskFormHandler);
 pageContentEl.addEventListener('click', taskButtonHandler);
-pageContentEl.addEventListener("change", taskStatusChangeHandler);
+pageContentEl.addEventListener('change', taskStatusChangeHandler);
+pageContentEl.addEventListener('dragstart', dragTaskHandler)
+pageContentEl.addEventListener('dragover', dropZoneDragHandler);
